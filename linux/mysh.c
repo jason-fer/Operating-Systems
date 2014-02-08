4476cd09a7a8dad3dc0ef62bfe62e78d0fe5fef9
Rohit Jain <rjain@cs.wisc.edu>
2014-02-08 15:54:40 -0600

    Small changes to support ls and exit

--- linux/mysh.c
+++ linux/mysh.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 void
 usage(char *prog) 
@@ -18,26 +19,44 @@ usage(char *prog)
   exit(1);
 }
 
-void shell() {
-  printf("mysh> \n");
+void 
+shell() 
+{
+  printf("mysh> ");
+}
+
+void 
+execute(char* line) 
+{
+  int rc = fork();
+
+  if (rc > 0) {
+    (void) wait(NULL);
+  } else if (rc == 0) {
+    char* argv[2];
+
+      if (strcmp(line,"ls") == 0) {
+        argv[0] = "/bin/ls";
+        argv[1] = NULL;
+        execvp(*argv, argv);
+      } else if (strcmp(line, "exit") == 0) {
+        exit(0);
+      }
+
+  } else {
+    perror("Error in Forking!\n");
+    exit(1);
+  }
 }
 
 int
-main(int argc, char *argv[]) {
-
-  int rc = fork();
-
-  if (rc == 0) {
+main(int argc, char *argv[]) 
+{
+  char line[512];
+  while (1) {
     shell();
-    (void) wait(NULL);
-  } else if (rc >= 0) {
-    printf ("I am here\n");
-    while(1) {
-      ;
-    }
-  } else {
-    perror("Error in Forking!\n");
+    scanf("%s", line); /* for now will change it to something else*/
+    execute(line);
   }
-
   return 0;
 }
