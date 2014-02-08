60f2401e4a74d355d8cb2156dbbc09c24d9c1865
Jason Feriante <jason.feriante@gmail.com>
2014-02-08 10:54:11 -0600

    initial commit

---
+++ linux/mysh.c
@@ -0,0 +1,24 @@
+/**
+ * Mysh
+ * <insert description here>
+ *
+ * Usage
+ * shell% ./fastsort -i inputfile -o outputfile
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+usage(char *prog) 
+{
+  fprintf(stderr, "Usage: fastsort -i inputfile -o outputfile\n");
+  exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+
+  return 0;
+}
