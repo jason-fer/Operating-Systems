0d8f5496acc714c89f05801aa1477607ccb9bff0
Rohit Jain <rjain@cs.wisc.edu>
2014-02-09 18:53:20 -0600

    Adding strtok capabilities. Need to do more

--- linux/mysh.c
+++ linux/mysh.c
@@ -26,58 +26,73 @@ prompt()
  * Compare two strings; ignore whitespace. First strips all white-space, then
  * return the comparison. 
  */
-int strcmpigwhite(const char *str1, const char *str2)
+
+void
+tokenize(char* line,
+         char** token)
 {
-  int x;
-  char *pstr1;
-  char *pstr2;
-  int max1 = strlen(str1);
-  int max2 = strlen(str2);
+  int i = 0;
+  token[i++] = strtok(line, " ");
 
-  // Add +1 for the null pointer
-  pstr1 = malloc(max1 + 1);
-  pstr2 = malloc(max2 + 1);
-
-  // @todo: check malloc
-
-  // Strip all white space characters from string 1
-  x = 0;
-  for (int i = 0; i < max1; ++i) {
-
-   if(str1[i] == '\0'){
-    // Found null; terminate the string & break the loop
-    pstr1[x] = str1[i];
-    break;
-   } else if (! isspace(str1[i])) {
-    // Copy over the non-whitespace item
-    pstr1[x] = str1[i];
-    x++;
-   }
+  while (token[i-1] != NULL) {
+    token[i++] = strtok(NULL, " ");
   }
-
-  // Strip all white space characters from string 2
-  x = 0;
-  for (int i = 0; i < max2; ++i) {
-
-   if(str2[i] == '\0'){
-    // Found null; terminate the string & break the loop
-    pstr2[x] = str2[i];
-    break;
-   } else if (! isspace(str2[i])) {
-    // Copy over the non-whitespace item
-    pstr2[x] = str2[i];
-    x++;
-   }
-  }
-
-  return strcmp(pstr1, pstr2);
 }
+
+/* int  */
+/* strcmpigwhite(const char *str1, const char *str2) */
+/* { */
+/*   int x; */
+/*   char *pstr1; */
+/*   char *pstr2; */
+/*   int max1 = strlen(str1); */
+/*   int max2 = strlen(str2); */
+/*   int i = 0; */
+
+/*   // Add +1 for the null pointer */
+/*   pstr1 = malloc(max1 + 1); */
+/*   pstr2 = malloc(max2 + 1); */
+
+/*   // @todo: check malloc */
+
+/*   // Strip all white space characters from string 1 */
+/*   x = 0; */
+/*   for (; i < max1; ++i) { */
+
+/*    if(str1[i] == '\0'){ */
+/*     // Found null; terminate the string & break the loop */
+/*     pstr1[x] = str1[i]; */
+/*     break; */
+/*    } else if (! isspace(str1[i])) { */
+/*     // Copy over the non-whitespace item */
+/*     pstr1[x] = str1[i]; */
+/*     x++; */
+/*    } */
+/*   } */
+
+/*   // Strip all white space characters from string 2 */
+/*   x = 0; */
+/*   for (i = 0; i < max2; ++i) { */
+
+/*    if(str2[i] == '\0'){ */
+/*     // Found null; terminate the string & break the loop */
+/*     pstr2[x] = str2[i]; */
+/*     break; */
+/*    } else if (! isspace(str2[i])) { */
+/*     // Copy over the non-whitespace item */
+/*     pstr2[x] = str2[i]; */
+/*     x++; */
+/*    } */
+/*   } */
+
+/*   return strcmp(pstr1, pstr2); */
+/* } */
 
 /**
  * Execute the provided command
  */
 void 
-execute(char* line) 
+execute(char** argv) 
 {
   int rc = fork();
 
@@ -88,29 +103,29 @@ execute(char* line)
 
   } else if (rc == 0) { // This is the child
     
-    Check for build in commands -- do we need to fork on these?
-    if (strcmpigwhite(line, "pwd") == 0) {
+    /*Check for build in commands -- do we need to fork on these?*/
+    if (strcmp(argv[0], "pwd") == 0) {
       char cwd[256];
       if (getcwd(cwd, sizeof(cwd)) == NULL) {
         // perror("pwd error!!!!"); // What should the error be?
       } else {
         printf("%s\n", cwd);
       }
-    } else if (strcmpigwhite(line, "cd") == 0) {
+    } else if (strcmp(argv[0], "cd") == 0) {
       const char* home = getenv("HOME");
       if(chdir(home) < 0){
         // perror("cd error!!!!"); // What should the error be?
       }
-    } else if (strcmpigwhite(line, "wait") == 0) {
+    } else if (strcmp(argv[0], "wait") == 0) {
       (void) wait(NULL); 
-    } /*else if (somefunc(line, "cd <somedir>")){
+    } /*else if (somefunc(argv[0], "cd <somedir>")){
       // [optionalSpace]cd[oneOrMoreSpace]dir[optionalSpace]
-      // if (strcmpigwhite(line, "cd dir")) // needs special handling
+      // if (strcmp(argv[0], "cd dir")) // needs special handling
     }*/
 
     // Standard commands probably need completely different handling.
     /* char* argv[2];
-    if (strcmp(line, "ls") == 0) {
+    if (strcmp(argv[0], "ls") == 0) {
       argv[0] = "/bin/ls";
       argv[1] = NULL;
       execvp(*argv, argv);
@@ -163,14 +178,16 @@ main(int argc, char *argv[])
     prompt();
 
     if(fgets(line, MAX_LINE_SIZE, stdin)){
+      
+      char* token[20];
+      tokenize(line, token);
 
-      if (strcmpigwhite(line, "exit") == 0) {
+      if (strcmp(token[0], "exit") == 0) {
         // Exit must be done outside the forked process
         exit(0);
       } else {
-        execute(line);
+        execute(token);
       }
-
     }
   }
 
