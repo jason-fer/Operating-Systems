0ce59b49dc2719f2617735a94d4bc26a300380e2
Jason Feriante <jason.feriante@gmail.com>
2014-03-29 17:31:22 -0500

    All usertests pass, but the bounds-test still doesn't pass.

--- p3/xv6/user/hello.c
+++ p3/xv6/user/hello.c
@@ -1,37 +1,64 @@
+/**
+ * Project 3B bounds test (I'm not sure why it's not passing) -Jason
+ */
+/* syscall argument checks (null page) */
 #include "types.h"
+#include "stat.h"
 #include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
 
 int
 main(int argc, char *argv[])
 {
+  char *arg;
 
-  // This should throw an error
-  int *p = NULL;
-  printf(1, "NULL Pointer addr: 0x%x\n", p);
-  printf(1, "De-reference Null Pointer: %d\n", *p);
-  printf(1, "De-reference Null Pointer: 0x%x\n", *p);
-
-  /*
-  // Top of the user addr space, minus first page
-  char *p = (char *) ((640 * 1024) - 4096);
-  // print addr as unsigned hex
-  printf(1, "addr of p: %x\n", p);
-  // attempt to write some data in this space
-  *p = 'f';
-
-  // confirm the address space gets copied correctly
-  int rc = fork();
-  if(rc == 0){
-    // Will trap because child wont get copy of high page
-    printf(1, "child: %c\n", *p);
-  } else if (rc > 0){
-    (void) wait();
-    printf(1, "parent: %c\n", *p);
-  } else {
-    printf(1, "bad thing happened");
+  int fd = open("tmp", O_WRONLY|O_CREATE);
+  // assert(fd != -1);
+  if(fd == -1){
+    printf(1, "assert failed (%s)\n", "fd != -1");
+    printf(1, "TEST FAILED\n");
+    exit();
   }
-  printf(1, "%c\n", *p);
-  */
- 
+
+  printf(1, "File opened\n");
+
+  // at zero
+  arg = (char*) 0x0;
+  // assert(write(fd, arg, 10) == -1);
+  if(write(fd, arg, 10) != -1){
+    printf(1, "assert failed (%s)\n", "write(fd, arg, 10)");
+    printf(1, "TEST FAILED\n");
+    exit();
+  }
+
+  printf(1, "File written 1\n");
+
+  // within null page
+  arg = (char*) 0x400;
+  // assert(write(fd, arg, 1024) == -1);
+  if(write(fd, arg, 1024) != -1){
+    printf(1, "assert failed (%s)\n", "write(fd, arg, 1024)");
+    printf(1, "TEST FAILED\n");
+    exit();
+  }
+
+  printf(1, "File written 2\n");
+   // spanning null page and code 
+  arg = (char*) 0xfff;
+  // assert(write(fd, arg, 2) == -1);
+  if(write(fd, arg, 2) != -1){
+    printf(1, "assert failed (%s)\n", "");
+    printf(1, "TEST FAILED\n");
+    exit();
+  }
+
+  printf(1, "File written 3\n");
+  // ensure stack is actually high...
+  //assert((uint) &local > 639*1024);
+
+  printf(1, "TEST PASSED\n");
   exit();
 }
