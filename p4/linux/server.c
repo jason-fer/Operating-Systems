cc5bacefc93de5a35e0684284294e3e8704e900a
Jason Feriante <jason.feriante@gmail.com>
2014-04-07 01:17:24 -0500

    Formatting

--- p4/linux/server.c
+++ p4/linux/server.c
@@ -14,45 +14,47 @@
 // CS537: Parse the new arguments too
 void getargs(int *port, int argc, char *argv[])
 {
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-	exit(1);
-    }
-    *port = atoi(argv[1]);
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+	*port = atoi(argv[1]);
 }
 
 
 int main(int argc, char *argv[])
 {
-    int listenfd, connfd, port, clientlen;
-    struct sockaddr_in clientaddr;
+	int listenfd, connfd, port, clientlen;
+	struct sockaddr_in clientaddr;
 
-    getargs(&port, argc, argv);
-
-    // 
-    // CS537: Create some threads...
-    //
-
-    listenfd = Open_listenfd(port);
-    while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+	getargs(&port, argc, argv);
 
 	// 
-	// CS537: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. However, for SFF, you may have to do a little work
-	// here (e.g., a stat() on the filename) ...
-	// 
-	requestHandle(connfd);
+	// CS537: Create some threads...
+	// A producer-consumer problem
+	// producer: listen & queue up requests
+	// consumer: pull requests out of queue & handle them
 
-	Close(connfd);
-    }
+	listenfd = Open_listenfd(port); // This opens a socket & listens on port #
+	while (1) {
+		clientlen = sizeof(clientaddr);
+		connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+
+		// 
+		// CS537: In general, don't handle the request in the main thread.
+		// Save the relevant info in a buffer and have one of the worker threads 
+		// do the work. However, for SFF, you may have to do a little work
+		// here (e.g., a stat() on the filename) ...
+		// 
+		requestHandle(connfd); 
+
+		Close(connfd);
+	}
 
 }
 
 
-    
+	
 
 
  
