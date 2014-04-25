161998dbb817c09cb101cdbe96a78133c3a96968
Jason Feriante <jason.feriante@gmail.com>
2014-04-24 21:07:40 -0500

    Setting up P5...

--- p5/client.c
+++ p5/client.c
@@ -7,25 +7,29 @@ char buffer[BUFFER_SIZE];
 int
 main(int argc, char *argv[])
 {
-    int sd = UDP_Open(0);
-    assert(sd > -1);
+	int sd = UDP_Open(0);
+	assert(sd > -1);
 
-    struct sockaddr_in saddr;
-    int rc = UDP_FillSockAddr(&saddr, "claudio.cs.wisc.edu", 10000);
-    assert(rc == 0);
+	struct sockaddr_in saddr;
+	// Once we UDP_FillSockAddr, we can use the socket over & over
+	int rc = UDP_FillSockAddr(&saddr, "claudio.cs.wisc.edu", 10000);
+	assert(rc == 0);
 
-    printf("CLIENT:: about to send message (%d)\n", rc);
-    char message[BUFFER_SIZE];
-    sprintf(message, "hello world");
-    rc = UDP_Write(sd, &saddr, message, BUFFER_SIZE);
-    // printf("CLIENT:: sent message (%d)\n", rc);
-    if (rc > 0) {
-	struct sockaddr_in raddr;
-	int rc = UDP_Read(sd, &raddr, buffer, BUFFER_SIZE);
-	printf("CLIENT:: read %d bytes (message: '%s')\n", rc, buffer);
-    }
+	printf("CLIENT:: about to send message (%d)\n", rc);
+	char message[BUFFER_SIZE];
+	sprintf(message, "hello world");
+	// e.g. 
+	// struct msg_t m;
+	// rc = UDP_Write(sd, &saddr, &m, sizeof(m));
+	rc = UDP_Write(sd, &saddr, message, BUFFER_SIZE);
+	// printf("CLIENT:: sent message (%d)\n", rc);
+	if (rc > 0) {
+		struct sockaddr_in raddr;
+		int rc = UDP_Read(sd, &raddr, buffer, BUFFER_SIZE);
+		printf("CLIENT:: read %d bytes (message: '%s')\n", rc, buffer);
+	}
 
-    return 0;
+	return 0;
 }
 
 
